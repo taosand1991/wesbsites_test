80d14454bacd27862469f4c9ed73e6cdb3af1960
Taofeek <taofeekdboy@yahoo.com>
2022-08-28 22:56:22 +0300

    fixed parallel test-3

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -51,12 +51,11 @@ export const config: Options.Testrunner = {
     // will be called from there.
     //
     specs: [
-        // [
-        //     './test/specs/homepage.spec.ts',
-        //     './test/specs/uitest.spec.ts',
-        //     './test/specs/saucelab.spec.ts',
-        // ]
-        './test/specs/homepage.spec.ts',
+        [
+            './test/specs/homepage.spec.ts',
+            './test/specs/uitest.spec.ts',
+            './test/specs/saucelab.spec.ts',
+        ]
     ],
     // Patterns to exclude.
     exclude: [
